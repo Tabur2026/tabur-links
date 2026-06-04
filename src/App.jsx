1dec9da097d602d6350936df7e61b87a27997af8
Tabur2026 <Coordinated@tabur.sa>
2026-06-04 17:44:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
     {
       title: "استمارة طلب فرنشايز طابور",
       subtitle: "",
-      icon: "/icons/doc.png",
+      icon: "/icons/franchiseReq.png",
       size:25,
       url: "https://docs.google.com/forms/d/1F6SvLTzFnni_MemrOVdHHOKXYRxCla3Iysa09dpB99M/viewform?edit_requested=true",
     },
